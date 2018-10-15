098ed94d7a7b5648032c73a99c03a57338b8d946
jimbo8399 <jimbo8399@gmail.com>
2018-10-15 14:23:58 +0100

    Fixed importing dataset 2 and added comments

--- bin/data_import.py
+++ bin/data_import.py
@@ -7,19 +7,29 @@ PATH_DATASET_2 = "./data/DATASET-2/pi"
 
 '''
 Imports a single sensor dataset from GNFUV
+A dataset from1 source with the columns:
+[id, humidity, temperature, experiment_id, time]
 Returns: pandas dataframe
 '''
 def import_sensor_from_dataset_2(path):
     with open(path,"r") as f:
-        line_of_data = next(f)
-        df = pd.read_json(json.dumps(line_of_data),lines=True)
+        line_of_data = next(f).strip()
+        line_of_data = line_of_data.replace("'",'"')
+        df = pd.read_json(line_of_data,lines=True,orient="records")
         for line_of_data in f:
-            temp_df = pd.read_json(json.dumps(line_of_data), lines=True)
+            line_of_data = line_of_data.strip()
+            line_of_data = line_of_data.replace("'",'"')
+            try:
+                temp_df = pd.read_json(line_of_data, lines=True, orient="records")
+            except ValueError:
+                continue
             df = df.append(temp_df)
     return df
 
 '''
-Imports the whole dataset from
+Imports the whole dataset from GNFUV
+Data contains datasets from 5 different sources
+Returns: a list with all pandas dataframes
 '''
 def import_dataset_2():
     df_pi2 = import_sensor_from_dataset_2(PATH_DATASET_2 + 
@@ -32,6 +42,12 @@ def import_dataset_2():
                          "5/gnfuv-temp-exp1-55d487b85b-5ztk8_1.0.csv")
     return [df_pi2, df_pi3, df_pi4, df_pi5]
 
+'''
+Import the whole dataset from the HT_Sensors
+A single dataset with the columns:
+[id, time, R1, R2, R3, R4, R5, R6, R7, R8, temperature, humidity]
+Return: pandas dataframe
+'''
 def import_dataset_1():
     with open(PATH_DATASET_1, "r") as file:
         columns = next(file).split()
@@ -39,4 +55,4 @@ def import_dataset_1():
     return df
 
 # if __name__=="__main__":
-#     import_dataset_1()
+#     import_dataset_2()
